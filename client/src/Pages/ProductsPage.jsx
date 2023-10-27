b66411c7038ab390dd5e0212050214e9a8df0e5a
Anas <anasrashidshah@hmail.com>
2023-10-27 13:11:40 +0530

    product card updated

--- client/src/Pages/ProductsPage.jsx
+++ client/src/Pages/ProductsPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { products } from '../products'
 
-import { Text,Center, Wrap, WrapItem } from '@chakra-ui/react'
+import {Center, Wrap, WrapItem } from '@chakra-ui/react'
+import ProductCard from '../Components/ProductCard'
 
 const ProductsPage = () => {
   return (
-    <Wrap spacing={"30px"} justify={"center"} minHeight={"100vh"}>
+    <Wrap spacing={"20px"} justify={"center"} minHeight={"100vh"}>
         {products.map((product) => (
             <WrapItem key={product._id}>
-                <Center w={"250px"} h={"550px"}>
-                    <img src={product.image}/>
-                    {/* <Text>{product.name}</Text> */}
+                <Center w={"250px"} h={"550px"} >
+                   <ProductCard product = {product}/>
                 </Center>
             </WrapItem>
         ))}
